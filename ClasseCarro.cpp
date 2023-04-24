976257684c9b45cf2c85d7f709ac6dc2fc0cc018
Shinji Nakano <127798992+Vharkus@users.noreply.github.com>
2023-04-24 19:54:22 -0300

    Update

--- ClasseCarro.cpp
+++ ClasseCarro.cpp
@@ -9,10 +9,18 @@ class Carro
     string Modelo;
     float VelMax;
     void Buzinar();
+    
+    Carro()
+    {
+        VelMax = 220.f;
+        Modelo = "Camaro";
+        Cor = "Amarelo";
+    }
 };
 int main() 
 {
     Carro MeuCarro;
+    Carro Carro2;
     
     MeuCarro.Cor = "Branco";
     MeuCarro.Modelo = "Fusca";
@@ -22,6 +30,11 @@ int main()
     cout << MeuCarro.Cor << endl;
     cout << MeuCarro.Modelo << endl;
     cout << MeuCarro.VelMax << endl;
+    
+    Carro2.Buzinar();
+    cout << Carro2.Cor << endl;
+    cout << Carro2.VelMax << endl;
+    cout << Carro2.Modelo << endl;
 }
 
 void Carro::Buzinar (){
